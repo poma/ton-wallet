fc2d31d8ec336255c9951e81a499a964413a8983
poma <semenov.roma@gmail.com>
2019-12-14 20:36:43 +0700

    update

--- bot/index.js
+++ bot/index.js
@@ -1,8 +1,30 @@
+require('dotenv').config()
 const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.command('start', (ctx) =>
-{
-    ctx.reply('Hello')
+
+const inlineMenu = Telegraf.Extra
+    .markdown()
+    .markup((m) => m.inlineKeyboard([
+        m.callbackButton('👛 Create wallet', '/wallet'),
+        m.callbackButton('✉️ Send transaction', '/transaction'),
+        m.callbackButton('🔑 Recover key', '/key')
+    ]).resize())
+
+const mainMenu = Telegraf.Extra
+    .markdown()
+    .markup((m) => m.keyboard([
+        m.callbackButton('💰 Get more money', '/money'),
+    ]).resize())
+
+bot.command('start', (ctx) => {
+    //ctx.reply('', mainMenu)
+    ctx.reply('Hello, select an action', inlineMenu)
 })
+
+bot.command('create', (ctx) => {
+    ctx.replyWithMarkdown('*Tx:* 0x00dead00beef\n*Amount:* 10 nanoGrams')
+})
+
 bot.launch()
---
+++ ui/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+app.get('/', function (req, res) {
+    // just for testing purposes
+    res.send('hello')
+})
+
+app.listen(process.env.PORT || 8000)
